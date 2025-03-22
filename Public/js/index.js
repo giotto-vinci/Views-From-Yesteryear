14d31b2648eab7c271a0bb0584323745b9a858c6
Roderick Fitzgerald <liber6schol@outlook.com>
2025-03-22 14:59:25 -0400

    re-installed package.json, express, and installed nodemon. Local host is now running and began index.jswithin public folder.

---
+++ Public/js/index.js
@@ -0,0 +1,8 @@
+"use strict"
+
+const el = {
+    style: document.getElementById("style"),
+    art: document.getElementById("art"),
+    artist: document.getElementById("name"),
+    piece: document.getElementById("title"),
+}
